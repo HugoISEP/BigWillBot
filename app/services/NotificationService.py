909b3cc23260d2222044b5d4cfe6cf93b5067ee1
hugo <hchemillier@juniorisep.com>
2022-01-16 13:19:24 +0100

    add firebase notifications

---
+++ app/services/NotificationService.py
@@ -0,0 +1,32 @@
+from configparser import ConfigParser
+
+from app.models.Notification import Notification
+from app.services.ApiService import ApiService
+from app.services.Utils import Utils
+
+path_to_configuration_file = "config.ini"
+
+
+class NotificationService:
+    _api_service: ApiService
+
+    def __init__(self):
+        config_init = ConfigParser()
+        config_init.read(path_to_configuration_file)
+        config = config_init["DEFAULT"]
+        api_url = config["api_base_url"]
+        self._api_service = ApiService(api_url)
+
+    def send_buy_notification(self, coin: str, amount: float, price: float):
+        notification = Notification(
+            title="Buy order",
+            body=f"buy order for {amount} {coin} at {price} $",
+            topic=Utils.NOTIFICATION_TOPIC)
+        self._api_service.post_notification(notification)
+
+    def send_sell_notification(self, coin: str, amount: float, price: float):
+        notification = Notification(
+            title="Sell order",
+            body=f"sell order for {amount} {coin} at {price} $",
+            topic=Utils.NOTIFICATION_TOPIC)
+        self._api_service.post_notification(notification)
